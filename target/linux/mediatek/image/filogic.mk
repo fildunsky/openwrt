f879a9053279b8f90d68d4c7f71cf06143c497df
Fil Dunsky <filipp.dunsky@gmail.com>
2026-08-17 17:09:04 +0300

    mediatek: filogic: netcore_n30-pro: drive USB LED from usbport trigger
    Wire the USB LED to the xhci ports with the usbport DT trigger like on
    netis NX32U instead of the board.d usbport config, and add
    kmod-usb-ledtrig-usbport to DEVICE_PACKAGES so the trigger is
    actually available in the image.
    
    Signed-off-by: Fil Dunsky <filipp.dunsky@gmail.com>

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -2651,7 +2651,8 @@ define Device/netcore_n30-pro
   DEVICE_DTS_DIR := ../dts
   DEVICE_DTC_FLAGS := --pad 4096
   DEVICE_DTS_LOADADDR := 0x43f00000
-  DEVICE_PACKAGES := kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware kmod-usb3
+  DEVICE_PACKAGES := kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware \
+	kmod-usb3 kmod-usb-ledtrig-usbport
   KERNEL_LOADADDR := 0x44000000
   KERNEL := kernel-bin | libdeflate-gzip
   KERNEL_INITRAMFS := kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
